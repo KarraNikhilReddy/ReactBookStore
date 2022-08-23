2d614ca8cb73204799e9e4f302b180fce466b2f6
Nikhil Reddy Karra <45452215+KarraNikhilReddy@users.noreply.github.com>
2022-08-23 13:37:44 +0530

    Update timelineTypes.js

--- src/redux/timeline/timelineTypes.js
+++ src/redux/timeline/timelineTypes.js
@@ -3,4 +3,4 @@
 export const SET_TIMELINE = "SET_TIMELINE"
 
 
-export const TIMELINE_API= 'http://localhost:8001/timeline'
+export const TIMELINE_API= 'https://my-json-server.typicode.com/KarraNikhilReddy/db/timeline'
