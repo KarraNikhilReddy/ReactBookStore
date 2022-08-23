12799057d30ae3a0ebd15a00c9fd3be2fd88bb56
Nikhil Reddy Karra <45452215+KarraNikhilReddy@users.noreply.github.com>
2022-08-23 13:36:12 +0530

    Update bookTypes.js

--- src/redux/book/bookTypes.js
+++ src/redux/book/bookTypes.js
@@ -13,12 +13,17 @@ export const RESET_CURRENT_BOOK = "RESET_CURRENT_BOOK"
 export const SET_BOOKS = "SET_BOOKS"
 
 
-export const BOOKS_API= 'http://localhost:8000/books'
+// export const BOOKS_API= 'http://localhost:8000/books'
 
-export const TIMELINE_API = "http://localhost:8001/timeline"
+// export const TIMELINE_API = "http://localhost:8001/timeline"
 
-export const BOOK_CATEGORIES_API= 'http://localhost:8000/book_categories'
-
-
+// export const BOOK_CATEGORIES_API= 'http://localhost:8000/book_categories'
 
 
+
+
+export const BOOKS_API= 'https://my-json-server.typicode.com/KarraNikhilReddy/db/books'
+
+export const TIMELINE_API = "https://my-json-server.typicode.com/KarraNikhilReddy/db/timeline"
+
+export const BOOK_CATEGORIES_API= 'https://my-json-server.typicode.com/KarraNikhilReddy/db/book_categories'
